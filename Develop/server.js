067fe1c357d9439ca2139be80ff08886881ebb26
rpounceyjr <rpounceyjr@gmail.com>
2020-04-20 16:57:47 -0400

    post works, writes an object to db.json.

--- Develop/server.js
+++ Develop/server.js
@@ -10,16 +10,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-// app.get("/api/notes", function (req, res) {
-//     const notes = fs.readFile(__dirname + "/Develop/db/db.json", function (err) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log("File read.")
-//         }
-//     })
-//     return res.json(notes);
-// });
+app.get("/api/notes", function (req, res) {
+    const notes = fs.readFile(__dirname + "/db/db.json", function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("File read.")
+            return notes
+        }
+    })
+    return res.json(notes);
+});
+// * POST `/api/notes` - Should receive a new note to save on the 
+//request body, add it to the `db.json` file, and then return 
+//the new note to the client.
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    
+    console.log(newNote);
+    fs.appendFile(__dirname + "/db/db.json", JSON.stringify(newNote), function(err){
+        if(err){
+            console.log(err)
+        }else{
+            console.log("Note taken")
+        }
+    } )
+
+
+    res.json(newNote);
+})
 
 //This is the server listener
 app.listen(PORT, function () {
