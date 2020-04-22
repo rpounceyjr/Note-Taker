f28673a492e6ed730c1ae5318caa59af5f0fa252
rpounceyjr <rpounceyjr@gmail.com>
2020-04-21 22:07:13 -0400

    solved a few problems.  Added express.static(public) which solved an issue with accessing the correct css/js.  Changed encoding on readFile so db.json is rendered correcty.

--- Develop/server.js
+++ Develop/server.js
@@ -6,33 +6,26 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 //Routes
-//HTML route for index.html
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/index.html"))
-});
-
-//HTML route for notes.html
-app.get("/notes.html", function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/notes.html"))
-});
-
 //API route for getting notes
 app.get("/api/notes", function (req, res) {
 
-    fs.readFile("/db/db.json", function (err, data) {
+    fs.readFile(__dirname + "/db/db.json", "utf-8", function (err, data) {
+        console.log(data)
         if (err) {
             console.log(err);
         } else {
             console.log("Notes obtained");
         }
         return res.json(data);
-
     })
 })
+
 //API route for posting notes
 app.post("/api/notes", function (req, res) {
     var newNote = req.body;
@@ -47,6 +40,17 @@ app.post("/api/notes", function (req, res) {
     })
     return res.json(newNote);
 });
+
+//HTML route for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/notes.html"));
+});
+//HTML route for index.html
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+});
+
+
 
 //This is the server listener
 app.listen(PORT, function () {
