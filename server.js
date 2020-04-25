86a769e896fa9724350bee16b67231e609dda1ca
rpounceyjr <rpounceyjr@gmail.com>
2020-04-25 14:52:01 -0400

    made more useful comments on the code.

--- server.js
+++ server.js
@@ -8,18 +8,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//This code fixed an error that caused an html page to load as the js page
 app.use(express.static("public"));
 
 //Routes
 //API route for getting notes
+//this route is called upon page load and notes are appended to the left side of the
+//notes page
 app.get("/api/notes", function (req, res) {
-
+    //fs.readFile is used to get the data from the JSON file
     fs.readFile(__dirname + "/db/db.json", "utf-8", function (err, data) {
         console.log(data)
         if (err) {
             console.log(err);
         } else {
-            console.log("Notes obtained");
+            console.log("Notes obtained.");
             res.send(JSON.parse(data));
         }
     })
@@ -27,23 +30,27 @@ app.get("/api/notes", function (req, res) {
 })
 
 //API route for posting notes
+//this is the route that is used when the user saves a note
 app.post("/api/notes", function (req, res) {
     let newNote = req.body;
+    //this line creates a unique ID for the new notes
     newNote.id = Date.now().toString();
+    //information from the JSON file is read and pushed to this array
     const notesArrayForPost = [];
-
-    console.log(newNote);
 
     fs.readFile(__dirname + "/db/db.json", "utf-8", function (err, data) {
         if (err) {
             console.log(err);
         } else {
-            let formattedDataForPost = JSON.parse(data)
+            let formattedDataForPost = JSON.parse(data);
+            //after reading the data, each entry in the JSON is pushed to 
+            //the notesArrayForPost array
             for (let i = 0; i < formattedDataForPost.length; i++) {
                 notesArrayForPost.push(formattedDataForPost[i]);
             }
         }
         notesArrayForPost.push(newNote);
+        //notesArrayForPost is written to db.json
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notesArrayForPost), function (err) {
             if (err) {
                 console.log(err)
@@ -67,14 +74,19 @@ app.delete("/api/notes/:id", function (req, res) {
             console.log(err);
         } else {
             let formattedDataForDelete = JSON.parse(data);
+            //if the id matches the id of a note in storage, that note is saved to 
+            //the variable noteToDelete
             for (let i = 0; i < formattedDataForDelete.length; i++) {               
-                if (id === formattedDataForDelete[i].id) {
+                if (id === formattedDataForDelete[i].id) {     
                     noteToDelete = formattedDataForDelete[i];
                 } else {
+             //if the id doesn't match the id of a stored note, that not is pushed
+             //to the notesToKeepArray to be written back to the db.json
                     notesToKeepArray.push(formattedDataForDelete[i])
                 }
             }
         }
+        //notesToKeepArray is written back to the db.json
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notesToKeepArray), function (err) {
             if (err) {
                 console.log(err)
